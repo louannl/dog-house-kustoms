5bf5a5b384c968d3b18bb65a58fe26bde942ad07
louannl <louann_loizou@hotmail.com>
2021-07-26 11:34:13 +0100

    added basic styling to contact and team page

--- src/pages/MeetTheTeam.js
+++ src/pages/MeetTheTeam.js
@@ -3,6 +3,13 @@ import Content from '../components/UI/Content';
 import classes from './MeetTheTeam.module.css';
 import lewis from '../assets/images/lewis.png';
 import linus from '../assets/images/linus.png';
+import Card from '../components/UI/card/Card';
+
+const description = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+enim ad minim veniam, quis nostrud exercitation ullamco laboris
+nisi ut aliquip ex ea commodo consequat.`;
 
 const MeetTheTeam = () => {
   return (
@@ -10,27 +17,13 @@ const MeetTheTeam = () => {
       <Element name="meet-the-team">
         <Content className={classes.content}>
           <h2>Meet the team</h2>
-          <div>
-            <div class={classes.photoCard}>
-              <img src={lewis} alt="lewis" />
-              <h3>Lewis Loizou</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </div>
-            <div class={classes.photoCard}>
-              <img src={linus} alt="linus" />
-              <h3>Linus Blazeys</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </div>
+          <div className={classes.teamCards}>
+            <Card image={lewis} name="Lewis Loizou" description={description} />
+            <Card
+              image={linus}
+              name="Linus Blazeys"
+              description={description}
+            />
           </div>
         </Content>
       </Element>
